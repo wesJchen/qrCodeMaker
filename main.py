c8dc557fa7fc7c27e477a1af4dc9abfb3e5a74a3
Wesley J Chen <102491101+wesJchen@users.noreply.github.com>
2025-01-20 10:29:55 -0500

    update phase one of app (#5)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import streamlit as st
 from PIL import Image
 
@@ -5,40 +6,69 @@ from qrmaker_script import QrMaker
 from helpers.style_helper import StyleHelper
 
 # Generate an image using the QrMaker function
-# qr_instance = QrMaker()
-# img_bytes = qr_instance.qr_save_byte("www.target.com")
+qr_instance = QrMaker()
 
 # to be deleted after testing this shiz
 img_name = "output/wesley_linkedin_qr.png"
 static_test_image = Image.open(img_name)
 
-# #Load the CSS Styles
-# StyleHelper.load_css("styles.css") #nevermind :<
+title_image = Image.open("assets/titleimage.png")
+warning_image = Image.open("assets/warningsign.png")
+
+### STYLES SETUP ###
 
 st.markdown(
     """
     <style>
     .container-class {
-        background-color: #6a0dad ;
-        padding: 20px;
+        background-color: white ;
+        padding: 15px;
         border-radius: 10px;
+        font-family: Menlo;
+        text-align: center;
+        color: black;
     }
+}
     </style>
     """,
     unsafe_allow_html=True,
 )
 
+### VARIABLES SETUP ###
+
+# Validate regex of the url link
+pattern = r"https://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/.*)?"
+
 # Generate containers to separate different areas of the app 
 header_container = st.container()
-container = st.container(border = True)
+image_container = st.container(border = True)
+url_container = st.container(border = True)
 
+### PAGE LAYOUT ###
+
+# Container for the header
 with header_container:
-    st.markdown('<div class="container-class">QR Image Maker</div>', unsafe_allow_html=True)
+    st.markdown('<div class="container-class"><strong>WESLEY\'S QR CODE MAKER</strong></div>',
+                unsafe_allow_html=True)
+    # st.image(title_image)
+
     StyleHelper.line_break(1)
 
-with container:
+# Container for the user URL input
+with url_container:
+    # Format URL input from user
+    title = st.text_input(label="URL Link", placeholder="Enter your url link here", disabled=False)
+    if re.fullmatch(pattern,title):
+        dynamic_image_byte = qr_instance.qr_save_byte(title)
+        st.write("Successfully generated!")
+    elif title:
+        st.write(f"Fail! The input `{title}`  is not valid. Enter a proper `https://` url.")
+    else:
+        st.write(" ")
 
-    StyleHelper.line_break(5)
+# Container for the image output
+with image_container:
+    StyleHelper.line_break(2)
 
     # Format multiple columns on the sheet
     col1, col2, col3 = st.columns(3)
@@ -46,21 +76,15 @@ with container:
         StyleHelper.line_break(1)
 
     with col2:
-        st.image(static_test_image)
-        StyleHelper.line_break(5)
-
+        if re.fullmatch(pattern,title):
+            st.image(dynamic_image_byte) #used for the dynamic qr
+            # st.image(static_test_image)
+        else:
+            st.image(warning_image)
     with col3:
         StyleHelper.line_break(1)
-    # st.image(static_test_image)
 
-StyleHelper.line_break(3)
-
-# Format URL input from user
-title = st.text_input(label="URL Link", placeholder="Paste url link here", disabled=True)
-if title:
-    st.write("QR has been generated for", title)
-else:
-    st.write(" ")
-
-# #Input the bytes of the QR image generated
-# st.image(img_bytes)
+    # container footer text
+    st.markdown(f"<div style='text-align: center;'><strong>QR link:</strong> <i>{title}</i></div>",
+                unsafe_allow_html=True)
+    StyleHelper.line_break(3)
